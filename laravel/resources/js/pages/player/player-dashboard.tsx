fea6992f299159c1615c75bef594326a43f61328
johannesheins <johannes.heins@gmx.de>
2026-05-10 14:11:03 +0200

    Replace a tag with Link

--- laravel/resources/js/pages/player/player-dashboard.tsx
+++ laravel/resources/js/pages/player/player-dashboard.tsx
@@ -1,4 +1,4 @@
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import {UserRoundPlus, UserSearch} from 'lucide-react';
 import {player} from '@/routes';
@@ -10,14 +10,14 @@ export default function PlayerDashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <a href="/player/create" title="Spieler erstellen" className="flex flex-col justify-center items-center h-full">
+                        <Link href="/player/create" title="Spieler erstellen" className="flex flex-col justify-center items-center h-full">
                             <UserRoundPlus className={"size-10 icon-color"}/>
-                        </a>
+                        </Link>
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <a href="/player/search" title="Spieler suchen" className="flex flex-col justify-center items-center h-full">
+                        <Link href="/player/search" title="Spieler suchen" className="flex flex-col justify-center items-center h-full">
                             <UserSearch className={"size-10 icon-color"}/>
-                        </a>
+                        </Link>
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
