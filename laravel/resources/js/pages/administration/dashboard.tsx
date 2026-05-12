50e76a5ec6adc0d3b999791c90e7b1a3f7c7598b
johannesheins <johannes.heins@gmx.de>
2026-05-12 23:34:39 +0200

    Add link to user-group-index.ts on administration dashboard

--- laravel/resources/js/pages/administration/dashboard.tsx
+++ laravel/resources/js/pages/administration/dashboard.tsx
@@ -1,6 +1,8 @@
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import {administration} from '@/routes';
+import {Users} from "lucide-react";
+import userGroup from "@/routes/administration/user-group";
 
 export default function Dashboard() {
     return (
@@ -9,7 +11,10 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <Link href={userGroup.index()} title="Benutzergruppen" className="flex flex-col gap-2 justify-center items-center h-full">
+                            <Users className={"size-10 icon-color"}/>
+                            <p className="text-icon-color font-bold">Benutzergruppen</p>
+                        </Link>
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
