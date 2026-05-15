a396c3ff840af9258df23bf695d2afe8f7d64fec
johannesheins <johannes.heins@gmx.de>
2026-05-15 15:33:39 +0200

    Make AdministrationTest abstract to prevent PHPUnit no-tests warning

--- laravel/tests/Feature/Administration/AdministrationTest.php
+++ laravel/tests/Feature/Administration/AdministrationTest.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class AdministrationTest extends TestCase
+abstract class AdministrationTest extends TestCase
 {
     use RefreshDatabase;
 
