834d291c931da4a9873d611490c910b9188183f3
johannesheins <johannes.heins@gmx.de>
2026-05-16 15:21:15 +0200

    Change name from block component to access-guard

--- laravel/resources/js/components/access-guard.tsx
+++ laravel/resources/js/components/access-guard.tsx
@@ -1,9 +1,7 @@
-//TODO Adjust name
-//TODO Add a title when disabled with e.g No right to search a player
 import React from 'react';
 import {cn} from "@/lib/utils";
 
-export default function Block({children, active, title}: {children: React.ReactNode, active?: boolean, title?: string}){
+export default function AccessGuard({children, active, title}: {children: React.ReactNode, active?: boolean, title?: string}){
     return (
         <div className="grid grid-cols-1 grid-rows-1 h-full w-full">
             {!active && <div title={title} className="z-10 col-start-1 row-start-1">
--- laravel/resources/js/pages/player/player-index.tsx
+++ laravel/resources/js/pages/player/player-index.tsx
@@ -2,7 +2,7 @@ import {Head, Link} from '@inertiajs/react';
 import {UserRoundPlus, UserSearch} from 'lucide-react';
 import PlayerSearchForm from "@/pages/player/player-search-form";
 import player from "@/routes/player";
-import Block from "@/components/block";
+import AccessGuard from "@/components/access-guard";
 import {useHasRight} from "@/hooks/use-has-right";
 import {RightEnum} from "@/enums";
 
@@ -16,20 +16,20 @@ export default function PlayerIndex() {
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-2">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <Block active={canCreate} title="Keine Berechtigung">
+                        <AccessGuard active={canCreate} title="Keine Berechtigung">
                             <Link href={player.create()} title="Spieler erstellen" className="flex flex-col gap-2 justify-center items-center h-full">
                                 <UserRoundPlus className={"size-10 icon-color"}/>
                                 <p className="text-icon-color font-bold">Spieler erstellen</p>
                             </Link>
-                        </Block>
+                        </AccessGuard>
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <Block active={canSearch} title="Keine Berechtigung">
+                        <AccessGuard active={canSearch} title="Keine Berechtigung">
                             <Link href={player.search()} title="Spieler suchen" className="flex flex-col gap-2 justify-center items-center h-full">
                                 <UserSearch className={"size-10 icon-color"}/>
                                 <p className="text-icon-color font-bold">Spieler suchen</p>
                             </Link>
-                        </Block>
+                        </AccessGuard>
                     </div>
                 </div>
                 {canSearch && <div className="content-center invisible md:visible relative min-h-screen flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
