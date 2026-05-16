79e72ef10a1bb6cbbe8f446b0d544b5e20bd7371
johannesheins <johannes.heins@gmx.de>
2026-05-17 00:05:32 +0200

    Add try catch and successfully message to GenerateTypescriptRightEnum.php

--- laravel/app/Console/Commands/GenerateTypescriptRightEnum.php
+++ laravel/app/Console/Commands/GenerateTypescriptRightEnum.php
@@ -16,19 +16,25 @@ class GenerateTypescriptRightEnum extends Command
      */
     public function handle()
     {
-        $dir = resource_path('js/enums');
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
+        try{
+            $dir = resource_path('js/enums');
+            if (!is_dir($dir)) {
+                mkdir($dir, 0755, true);
+            }
 
-        $fileContent = "export enum RightEnum {\n";
-        foreach(RightEnum::cases() as $enum){
-            $fileContent .= "   {$enum->name} = {$enum->value},\n";
-        }
-        $fileContent .= "}";
+            $fileContent = "export enum RightEnum {".PHP_EOL;
+            foreach(RightEnum::cases() as $enum){
+                $fileContent .= "   {$enum->name} = {$enum->value},".PHP_EOL;
+            }
+            $fileContent .= "}";
 
-        file_put_contents("{$dir}/right-enum.ts", $fileContent);
-        file_put_contents("{$dir}/index.ts", "export { RightEnum } from './right-enum';\n");
+            file_put_contents("{$dir}/right-enum.ts", $fileContent);
+            file_put_contents("{$dir}/index.ts", "export { RightEnum } from './right-enum';".PHP_EOL);
+
+            echo 'The right-enum.ts was successfully created'.PHP_EOL;
+        }catch(\Exception $e){
+            echo $e->getMessage();
+        }
     }
 
 
