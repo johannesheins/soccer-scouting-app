375dffa1fa1fa7de8618200c638ad3eeff1732ca
johannesheins <johannes.heins@gmx.de>
2026-06-05 07:31:19 +0200

    Correct table name in migration down

--- laravel/database/migrations/2026_05_16_145759_create_evaluation_criterias_table.php
+++ laravel/database/migrations/2026_05_16_145759_create_evaluation_criterias_table.php
@@ -17,6 +17,6 @@ return new class extends Migration {
 
     public function down(): void
     {
-        Schema::dropIfExists('evaluation_criterias');
+        Schema::dropIfExists('evaluation_criteria');
     }
 };
