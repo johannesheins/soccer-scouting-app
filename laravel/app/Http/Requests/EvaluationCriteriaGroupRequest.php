fe76af7fda521936fd68676696b9ae85ba09c0fd
johannesheins <johannes.heins@gmx.de>
2026-07-02 22:43:34 +0200

    Ignore current record in name at unique check on update

--- laravel/app/Http/Requests/EvaluationCriteriaGroupRequest.php
+++ laravel/app/Http/Requests/EvaluationCriteriaGroupRequest.php
@@ -3,13 +3,14 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class EvaluationCriteriaGroupRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'unique:evaluation_criteria_groups,name'],
+            'name' => ['required', 'string', 'max:255', Rule::unique('evaluation_criteria_groups')->ignore($this->route('evaluation_criteria_group'))],
         ];
     }
 
--- laravel/app/Http/Requests/EvaluationCriteriaRequest.php
+++ laravel/app/Http/Requests/EvaluationCriteriaRequest.php
@@ -3,13 +3,14 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class EvaluationCriteriaRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'unique:evaluation_criteria,name'],
+            'name' => ['required', 'string', 'max:255', Rule::unique('evaluation_criteria', 'name')->ignore($this->route('evaluation_criterion'))],
             'minimum_player_age' => ['nullable', 'integer'],
             'multiplier' => ['required', 'integer'],
             'evaluation_criteria_group_id' => ['nullable', 'integer', 'exists:evaluation_criteria_groups,id'],
